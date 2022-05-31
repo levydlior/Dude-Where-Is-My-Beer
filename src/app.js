a770d0cb48a1e392e98b5a269390f8ebfc3db43d
Lior <levydlior@gmail.com>
2022-05-31 15:35:07 -0600

    fixed zip fetch

--- src/app.js
+++ src/app.js
@@ -2,13 +2,12 @@ let zipCode = "";
 const zipForm = document.querySelector("#form-by-zip");
 const dropDown = document.querySelector("#brew-drop-down");
 
-const zipURL = `https://api.openbrewerydb.org/breweries?by_postal=${zipCode}&per_page=100`;
-
 function fetchZipBrew() {
-  fetch(zipURL)
+  fetch(
+    `https://api.openbrewerydb.org/breweries?by_postal=${zipCode}&per_page=25`
+  )
     .then((response) => response.json())
     .then((brewries) => {
-      dropDown.replaceChildren()
       brewries.forEach((brewery) => {
         renderBrewToDropDown(brewery);
       });
@@ -26,6 +25,8 @@ zipForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const zipInput = document.querySelector("#zip-code");
   zipCode = zipInput.value;
+  dropDown.replaceChildren();
+
   fetchZipBrew();
   zipForm.reset();
 });
