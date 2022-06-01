c1e445b8c1ea750653f9715b8711acdfebaea275
Lior <levydlior@gmail.com>
2022-05-31 19:30:08 -0600

    blank option after zip fetch

--- src/app.js
+++ src/app.js
@@ -1,27 +1,5 @@
-
-const details = document.querySelector('#brewry-details')
-const randomBtn = document.querySelector('#random-brewery-button')
-
-const renderBreweryDetails = brewery => {
-
-    const cardDetails = document.createElement('div')
-    const breweryName = document.createElement('h1')
-    const breweryStreet = document.createElement('h2')
-    const breweryCity = document.createElement('h3')
-    const breweryState = document.createElement('h4')
-    const breweryZipCode = document.createElement('h5')
-    const breweryWebsite = document.createElement('h6')
-
-    breweryName.textContent = brewery.name
-    breweryStreet.textContet = brewery.street
-    breweryCity.textContent = brewery.city
-    breweryState.textContent = brewery.state
-    breweryZipCode.textContent = brewery.postal_code
-    breweryWebsite.textContent = brewery.website_url
-
-    cardDetails.append(breweryName, breweryStreet, breweryCity, breweryState, breweryZipCode, breweryWebsite)
-    details.append(cardDetails)
-}
+const details = document.querySelector("#brewry-details");
+const randomBtn = document.querySelector("#random-brewery-button");
 
 let zipCode = "";
 const zipForm = document.querySelector("#form-by-zip");
@@ -33,6 +11,8 @@ function fetchZipBrew() {
   )
     .then((response) => response.json())
     .then((brewries) => {
+      const emptyOption = document.createElement("option");
+        dropDown.append(emptyOption)
       brewries.forEach((brewery) => {
         renderBrewToDropDown(brewery);
       });
@@ -41,6 +21,7 @@ function fetchZipBrew() {
 
 function renderBrewToDropDown(brewery) {
   const brewOptions = document.createElement("option");
+  
   brewOptions.textContent = brewery.name;
   brewOptions.value = brewery.name;
   dropDown.append(brewOptions);
@@ -56,22 +37,44 @@ zipForm.addEventListener("submit", (e) => {
   zipForm.reset();
 });
 
-
 function fetchRandomBrewery() {
-    fetch(
-      'https://api.openbrewerydb.org/breweries/random'
-    )
-      .then((response) => response.json())
-      .then((brewries) => {
-        brewries.forEach((brewery) => {
-          renderBreweryDetails(brewery);
-        })
-      })
-  }
+  fetch("https://api.openbrewerydb.org/breweries/random")
+    .then((response) => response.json())
+    .then((brewries) => {
+      brewries.forEach((brewery) => {
+        renderBreweryDetails(brewery);
+      });
+    });
+}
 
-  randomBtn.addEventListener('click', () => {
+randomBtn.addEventListener("click", () => {
+  fetchRandomBrewery();
+  details.replaceChildren();
+});
 
-    fetchRandomBrewery()
-    details.replaceChildren()
+const renderBreweryDetails = (brewery) => {
+  const cardDetails = document.createElement("div");
+  const breweryName = document.createElement("h1");
+  const breweryStreet = document.createElement("h2");
+  const breweryCity = document.createElement("h3");
+  const breweryState = document.createElement("h4");
+  const breweryZipCode = document.createElement("h5");
+  const breweryWebsite = document.createElement("h6");
 
-  })
+  breweryName.textContent = brewery.name;
+  breweryStreet.textContet = brewery.street;
+  breweryCity.textContent = brewery.city;
+  breweryState.textContent = brewery.state;
+  breweryZipCode.textContent = brewery.postal_code;
+  breweryWebsite.textContent = brewery.website_url;
+
+  cardDetails.append(
+    breweryName,
+    breweryStreet,
+    breweryCity,
+    breweryState,
+    breweryZipCode,
+    breweryWebsite
+  );
+  details.append(cardDetails);
+};
